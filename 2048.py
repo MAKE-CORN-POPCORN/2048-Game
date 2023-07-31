e0045b9270793fa1bdc93d91228eeaf3ebb70535
POPING-POPCORN-REAL <140892114+POPING-POPCORN-REAL@users.noreply.github.com>
2023-07-31 14:20:58 +0800

    Finish 2048 Game
     - Fixing The Previous Error
     - Add "Score" And "MoveStep" To Game

--- 2048.py
+++ 2048.py
@@ -37,7 +37,7 @@ def arr__getElementsAlongAxis(axis, cubic):
             [print('\t', c) for c in cubic]
         return cubic
     elif axis=='U2D':
-        cubic = [[cubic[i][-1-j] for i in range(len(cubic))] for j in range(len(cubic[0]))]
+        cubic = [[cubic[-1-i][-1-j] for i in range(len(cubic))] for j in range(len(cubic[0]))]
         if debug:
             print('\treturn')
             [print('\t', c) for c in cubic]
@@ -45,6 +45,8 @@ def arr__getElementsAlongAxis(axis, cubic):
     return cubic
 def arr__combineAlongAxis(arr):
     global debug
+    global score
+    global adder
     if debug:
         print('arr__combineAlongAxis')
         [print(c) for c in cubic]
@@ -55,6 +57,8 @@ def arr__combineAlongAxis(arr):
     # print(arr)
     while i<n-1:
         if arr[i]==arr[i+1]:
+            adder += arr[i+1]+arr[i]
+            score += adder
             arr[i]+=arr[i+1]
             arr[i+1]=0
             i+=2
@@ -92,20 +96,37 @@ def canPlay(cubic):
                 else:
                     x,y = i, j+a
                 if 0<=x and x<horizontalLim and 0<=y and y<verticalLim:
-                    if cubic[x][y]==cubic[i][j]:
+                    if debug:
+                        print(x, y, end='')
+                        print(cubic[y][x])
+                        print(i, j, end='')
+                        print(cubic[j][i], '\n\n')
+                    if cubic[y][x]==cubic[j][i] or (cubic[y][x]==0 or cubic[j][i]==0):
                         flag = True
                         break
     return flag
 def str__scriptedNum(num, max_len):
     num_len = int(np.log(10, num))
     return ' '*int(max_len-int(max_len/2))+str(num)+' '*int(max_len/2)
-def easy_updatePixel(cubic, round_):
-    os.system('cls')
+def easy_updatePixel(cubic):
+    global debug
+    global score
+    global adder
+    global round_
+    if not debug:
+        os.system('cls')
     if round_==0:
         print('==== START NEW GAME ====')
-    if round_==-2:
+    if round_<0:
         print('==== RESUME OLD GAME ====')
+        round_ = -round_
     [print(c) for c in cubic]
+    if adder==0:
+        print('\nScore: %d'%score)
+    else:
+        print('\nScore: %d(+%d)'%(score, adder))
+    print('Move: %d'%(round_))
+    adder = 0
     return round_+1 if round_!=-1 else -1
 def updatePixel(cubic):
     global horizontalLim
@@ -135,6 +156,8 @@ def updatePixel(cubic):
     ## next step
     print('\n\n\n\nNext Instrctor')
 def cubic__setInitState():
+    global horizontalLim
+    global verticalLim
     print('Input The Number And Seperate With Space, \'SPACE\' or \'0\' PLEASE TYPE \'0\', \'LAST\' For The Leaving...')
     cubic = []
     text = input('---->')
@@ -148,31 +171,67 @@ def cubic__setInitState():
     for line in cubic:
         if len(line)<max_len:
             line = line + [0]*(max_len-len(line))
+    horizontalLim = len(cubic[0])
+    verticalLim = len(cubic)
+    if debug:
+        print(horizontalLim, verticalLim)
     return cubic
+# 1024 0 0 4096
+# 512 256 128 0
+# 16 8 4 2
+# 2 2 2 2
+# LAST
+def getCubicScore():
+    global cubic
+    global score
+    global round_
+    numbers = []
+    extract_ = [[cubic[i][j] for i in range(len(cubic)) if cubic[i][j]!=0] for j in range(len(cubic[0]))]
+    for e in extract_:
+        numbers += e if e!=[] else []
+    if debug=='cubicScore':
+        print(numbers)
+    for n,e in zip(numbers, np.log2(numbers)):
+        if debug=='cubicScore':
+            print(n, e, n*(e-1), 2**(e-1)-1)
+        print(n, e, n*(e-1), 2**(e-1)-1)
+        score+=n*(e-1)
+        round_+=e
+    return score, round_
+
 debug = False
 horizontalLim, verticalLim = 4, 4
 initBlockNum = random.randint(1,3)
 mode = input('Play New Game (N) / Play Specific Game (S) / Exit (E)\nInput The Mode You Want ----->')
 cubic = None
+score = 0
+adder = 0
 round_ = 0
 if mode=='N':
     cubic = cubic__initBlocks(horizontalLim, verticalLim, initBlockNum)
 elif mode=='S':
-    round_ = -2
     cubic = cubic__setInitState()
+    score, round_ = getCubicScore()
 elif mode=='E':
     exit()
+new_cubic = None
 while canPlay(cubic):
     # updatePixel(cubic)
-    round_ = easy_updatePixel(cubic, round_)
+    round_ = easy_updatePixel(cubic)
     axis = pos__getAxisVector()
     # cubic = arr__getElementsAlongAxis(axis, [arr__combineAlongAxis(c) for c in arr__getElementsAlongAxis(axis, cubic)])
-    cubic = [arr__combineAlongAxis(c) for c in arr__getElementsAlongAxis(axis, cubic)]
+    new_cubic = [arr__combineAlongAxis(c) for c in arr__getElementsAlongAxis(axis, cubic)]
     if debug:
-        [print(c) for c in cubic]
-    cubic = arr__getElementsAlongAxis(axis, cubic)
-    cubic = cubic__addBlocks(horizontalLim, verticalLim, 1)
+        [print(c) for c in new_cubic]
+    new_cubic = arr__getElementsAlongAxis(axis, new_cubic)
+    if cubic!=new_cubic:
+        cubic = new_cubic
+        cubic = cubic__addBlocks(horizontalLim, verticalLim, 1)
+    else:
+        adder = 0
+        round_ -= 1
     if debug:
-        [print(c) for c in cubic]
+        [print(c) for c in new_cubic]
+print('==== GAME OVER ====')
 
 
